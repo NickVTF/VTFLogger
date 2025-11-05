e2e8f4b0d16805ad258f27c313c2ada9f2ba5354
lizhang4 <lizhang.tan@virtualtechfrontier.com>
2025-11-05 13:13:51 +0800

    updated some stuff

---
+++ Runtime/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using UnityEngine;
+using ILogger = UnityEngine.ILogger;
+
+namespace VTFLogger
+{
+    public class Test : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(logging => { logging.SetMinimumLevel(LogLevel.Trace); });
+        }
+    }
+}
