84067f0dd9b93f051ecf640797d676fb932d7304
lizhang4 <lizhang.tan@virtualtechfrontier.com>
2025-11-05 16:24:56 +0800

    update sink

---
+++ Runtime/ILogSink.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace VTFLogger.Runtime
+{
+    public interface ILogSink
+    {
+        void Initialize();
+        void Release();
+    }
+    
+    public class FileLogHandler : ILogSink
+    {
+        private StreamWriter writer;
+        private string logFilePath;
+        
+
+      
+        public void Initialize()
+        {
+            string logDir = Path.Combine(Application.persistentDataPath, "logs");
+            if (!Directory.Exists(logDir))
+                Directory.CreateDirectory(logDir);
+
+            string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            logFilePath = Path.Combine(logDir, $"{timestamp}.log");
+
+            Application.logMessageReceived += Log;
+            writer = new StreamWriter(logFilePath, append: false);
+            writer.AutoFlush = true;
+
+            writer.WriteLine($"=== Log session started: {DateTime.Now} ===");
+        }
+
+        
+
+        public void Release()
+        {
+            Application.logMessageReceived -= Log;
+            writer?.Close();
+        }
+        
+        private void Log(string message, string stackTrace, LogType type)
+        {
+            string line = $"[{DateTime.Now:HH:mm:ss}] [{type}] {message}";
+            writer.WriteLine(line);
+        }
+    }
+}
---
+++ Runtime/LogManager.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace VTFLogger.Runtime
+{
+    public static class LogManager
+    {
+        private static FileLogHandler _fileLogHandler;
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+            _fileLogHandler = new FileLogHandler();
+            _fileLogHandler.Initialize();
+        }
+
+        // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Release()
+        {
+            _fileLogHandler.Release();
+        }
+        
+    }
+}
